2129695d558ffef2a8f1ce51304ac566e1e09b76
Amritansh Soni <amritansh@Amritanshs-MacBook-Air.local>
2026-03-02 22:22:19 +0530

    First Commit

---
+++ src/main/java/com/libmgmt/libmgmt/LibmgmtApplication.java
@@ -0,0 +1,13 @@
+package com.libmgmt.libmgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibmgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibmgmtApplication.class, args);
+	}
+
+}
